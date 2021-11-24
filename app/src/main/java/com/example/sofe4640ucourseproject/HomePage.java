82437cfaa7687c7d3f031b7628bfe349f20367a8
Anuj Patel <anujmaitri@gmail.com>
2021-11-24 14:05:25 -0500

    Implemented Add a Friend Feature

--- app/src/main/java/com/example/sofe4640ucourseproject/HomePage.java
+++ app/src/main/java/com/example/sofe4640ucourseproject/HomePage.java
@@ -5,34 +5,79 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class HomePage extends AppCompatActivity {
     RecyclerView recyclerView;
     TextView userName;
     Button logOutUserBtn;
+    ImageButton addUser;
     String recyclerName[], recyclerDescription[];
+
+
+    private FirebaseAuth mAuth;
+
     int images[] = {R.drawable.userimage,R.drawable.userimage,R.drawable.userimage};
     @Override
     public void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
 
-        recyclerView = findViewById(R.id.recyclerView_friends);
-        recyclerName = new String[] {"Anuj","Ireni","Raj"};
-        recyclerDescription = new String[] {"welcome to Chat","welcome to Chat","welcome to Chat"};
-        UserAdapter myAdapter = new UserAdapter(this, recyclerName, recyclerDescription, images);
-        recyclerView.setAdapter(myAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        List<String> list = new ArrayList<>();
 
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
+        DocumentReference codesRef = rootRef.collection("Friends").document(currentUser.getEmail());
+        codesRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+
+                    Map<String, Object> map = task.getResult().getData();
+                    for (Map.Entry<String, Object> entry : map.entrySet()) {
+                        list.add(entry.getKey());
+
+
+                        Log.d("TAG", entry.getKey());
+                    }
+                    //Do what you want to do with your list
+                }else{
+                    System.out.println("Task was unsuccessful");
+                }
+                String[] stockArr = new String[list.size()];
+                stockArr = list.toArray(stockArr);
+                setupView(stockArr);
+            }
+        });
+
+
+
+
+        addUser = (ImageButton) findViewById(R.id.addUserButton);
+        //addUser.findViewById(R.id.addUserButton);
         logOutUserBtn = findViewById(R.id.logout_btn);
         userName = findViewById(R.id.user_name);
 
@@ -50,5 +95,21 @@ public class HomePage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        addUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomePage.this,addFriendPage.class));
+            }
+        });
+    }
+    public void setupView(String[] stockArr){
+        recyclerView = findViewById(R.id.recyclerView_friends);
+
+        recyclerName = stockArr;
+        recyclerDescription = new String[] {"welcome to Chat","welcome to Chat","welcome to Chat"};
+        UserAdapter myAdapter = new UserAdapter(this, recyclerName, recyclerDescription, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/example/sofe4640ucourseproject/addFriendPage.java
@@ -0,0 +1,88 @@
+package com.example.sofe4640ucourseproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
+
+public class addFriendPage extends AppCompatActivity {
+
+    Button addFriend;
+    EditText searchFriend;
+    String token;
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addfriendpage);
+
+        addFriend = findViewById(R.id.veriFriendButton);
+        searchFriend = findViewById(R.id.friendEmail);
+
+        addFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                token=searchFriend.getText().toString();
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                mAuth = FirebaseAuth.getInstance();
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+
+                DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("Users");
+                Query friendQuery = usersRef.orderByChild("email").equalTo(token);
+
+                friendQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot friendSnapshot : dataSnapshot.getChildren()) {
+                            System.out.println(friendSnapshot.getKey());
+                            System.out.println(friendSnapshot.child("name").getValue(String.class));
+                            if (dataSnapshot.getChildrenCount()>0) {
+                                Map<String, Object> friends = new HashMap<>();
+                                friends.put(token,friendSnapshot.child("name").getValue(String.class));
+                                db.collection("Friends").document(currentUser.getEmail())
+                                        .set(friends, SetOptions.merge());
+
+                                Toast.makeText(addFriendPage.this, friendSnapshot.child("name").getValue(String.class) + " was added!", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(addFriendPage.this,HomePage.class));
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        throw databaseError.toException();
+                    }
+                });
+            }
+        });
+
+    }
+}
